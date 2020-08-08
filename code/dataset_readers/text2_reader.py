153276db4c07d1c877e4e105bff609ac4d47230b
nastya <peganoff2@mail.ru>
2020-08-08 13:14:57 +0300

    modify text2_reader

--- code/dataset_readers/text2_reader.py
+++ code/dataset_readers/text2_reader.py
@@ -1,9 +1,13 @@
 import torch
 from pathlib import Path
-from torchtext import data.datasets.WikiText2
+from torchtext.datasets import WikiText2
+from torch.utils.data import DataLoader
 
 class Text2Reader():
 
     def read(self, data_path: str):
         data_path = Path(data_path)
-
+        
+        wikitext2_dataset = WikiText2(data_path)
+        
+        return wikitext2_dataset
