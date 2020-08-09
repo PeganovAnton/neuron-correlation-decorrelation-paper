619487006dfb9ef0185801d938f99b552fbe05dc
nastya <peganoff2@mail.ru>
2020-08-09 20:19:38 +0300

    Modify updating lr_scheduler, train, config

--- code/dataset_readers/text2_reader.py
+++ code/dataset_readers/text2_reader.py
@@ -1,12 +1,13 @@
 import torch
+import torchtext
 from pathlib import Path
 from torchtext.datasets import WikiText2
 from torch.utils.data import DataLoader
 
-class Text2Reader():
+class Text2Reader(torchtext.data.Dataset):
 
-    def read(self, data_path: str):
-        data_path = Path(data_path)
+    def __init__(self, data_path: str, split, preproc):
+        self.data_path = Path(data_path)
         
         wikitext2_dataset = WikiText2(data_path)
         
